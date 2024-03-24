3ec9b8cc9c680b16abf097403c1ab1ac67380de1
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-03-24 13:41:02 +0530

    added authsettings, and AuthorizeMiddleware

--- AuthLoom/Middleware/AuthorizeMiddleware.cs
+++ AuthLoom/Middleware/AuthorizeMiddleware.cs
@@ -1,6 +1,9 @@
-﻿using AuthLoom.Models;
+﻿using AuthLoom.Jwt;
+using AuthLoom.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Text.Json;
 
 namespace AuthLoom.Middleware
 {
@@ -11,8 +14,36 @@ namespace AuthLoom.Middleware
 
             var authConfig = configuration.GetSection("Auth");
             var authSettings = authConfig.Get<AuthSettings>();
+            var jwtInfo = GetTokenInfo(context, authSettings!.Jwt.Secret);
+            var authorize = await Authorize(context, jwtInfo);
+            if (authorize)
+            {
+                await next(context);
+            }
+            else
+            {
+                await SendUnauthorizedResponse(context);
+            }
+        }
 
-            await next(context);
+        private async Task<bool> Authorize(HttpContext context, JwtInfo jwtInfo)
+        {
+            return true;
+        }
+
+        private JwtInfo GetTokenInfo(HttpContext context, string secret)
+        {
+            var authHeader = context.Request.Headers["Authorization"].First() ?? throw new Exception("authorization header not found");
+            var token = authHeader.Replace("bearer", "").Trim();
+            return JwtUtil.GetTokenInfo(token, secret);
+        }
+
+        private async Task SendUnauthorizedResponse(HttpContext context)
+        {
+            string message = "user unauthorized to perform this action";
+            var response = JsonSerializer.Serialize(message);
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            await context.Response.WriteAsync(response);
         }
     }
 }
--- AuthLoom/Models/AuthSettings.cs
+++ AuthLoom/Models/AuthSettings.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AuthLoom.Models
+﻿namespace AuthLoom.Models
 {
     public class AuthSettings
     {
         public string BaseUrl { get; set; } = string.Empty;
         public bool SuperAdminEnabled { get; set; }
-        public List<Endpoint> Endpoints { get; set; } 
+        public List<Endpoint> Endpoints { get; set; } = new List<Endpoint>();
+        public Jwt Jwt { get; set; } = new();
     }
 }
